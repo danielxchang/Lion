8d333705897848ccdb1e9197073202ebfcafc3ff
Daniel Chang <daniel.changk@gmail.com>
2022-09-20 15:26:59 -0500

    implemented event listeners and added css modules

--- client/src/components/Challenge.js
+++ client/src/components/Challenge.js
@@ -1,34 +1,64 @@
 import { useState } from "react";
 import ItemRow from "./ItemRow";
-
-const sampleRow = {
-  sku: "12345",
-  itemName: "Gummies",
-  inStock: 10,
-  capacity: 30,
-};
+import styles from "./Challenge.module.css";
 
 export default function Challenge() {
-  const [totalCost, setTotalCost] = useState(null);
+  const [totalCost, setTotalCost] = useState(0);
   const [renderedRows, setRenderedRows] = useState(null);
-  const [restockAmounts, setRestockAmounts] = useState({});
+  const [restockAmounts, setRestockAmounts] = useState(null);
 
   const onLowStockClickHandler = () => {
-    // TODO: Implement requests out to low stock backend endpoint
-    const testRows = [
-      <ItemRow
-        key={sampleRow.sku}
-        {...sampleRow}
-        inputChange={onInputChangeHandler}
-      />,
-    ];
-    setRenderedRows(testRows);
+    fetch("http://localhost:4567/low-stock")
+      .then((res) => res.json())
+      .then((result) => {
+        const items = result.items;
+        const itemRows = items.map((item) => (
+          <ItemRow
+            key={item.sku}
+            {...item}
+            inputChange={onInputChangeHandler}
+          />
+        ));
+        const restockItems = items.reduce((prevObj, item) => {
+          prevObj[item.sku] = 0;
+          return prevObj;
+        }, {});
+
+        setRenderedRows(itemRows);
+        setRestockAmounts(restockItems);
+      })
+      .catch((err) => console.log(err));
   };
 
   const onReOrderClickHandler = () => {
-    // TODO: Implement requests out to reorder backend endpoint
-    console.log(restockAmounts);
-    setTotalCost(0);
+    if (!restockAmounts) {
+      alert(
+        'You must first click "Get Low-Stock Items" and input order amounts!'
+      );
+      return;
+    }
+
+    const data = {
+      items: Object.keys(restockAmounts).map((sku) => {
+        return {
+          sku,
+          quantity: restockAmounts[sku],
+        };
+      }),
+    };
+
+    fetch("http://localhost:4567/restock-cost", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setTotalCost(result.totalCost);
+      })
+      .catch((err) => console.log(err));
   };
 
   const onInputChangeHandler = (sku, amount) => {
@@ -51,23 +81,37 @@ export default function Challenge() {
           </tr>
         </thead>
         <tbody>
-          {renderedRows}
-          {/* 
-          TODO: Create an <ItemRow /> component that's rendered for every inventory item. The component
-          will need an input element in the Order Amount column that will take in the order amount and 
-          update the application state appropriately.
-          */}
+          {renderedRows &&
+            (renderedRows.length > 0 ? (
+              renderedRows
+            ) : (
+              <tr>
+                <td colSpan="5">NO LOW-STOCK ITEMS!</td>
+              </tr>
+            ))}
         </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="5" className={styles.total}>
+              Total Cost: ${`${totalCost.toFixed(2)}`}
+            </td>
+          </tr>
+        </tfoot>
       </table>
-      {/* TODO: Display total cost returned from the server */}
-      <div>
-        Total Cost: {totalCost === null ? "TBD" : `${totalCost.toFixed(2)}`}
+      <div className={styles.btnGroup}>
+        <button
+          className={`${styles.lowStockBtn} ${styles.btn}`}
+          onClick={onLowStockClickHandler}
+        >
+          Get Low-Stock Items
+        </button>
+        <button
+          className={`${styles.reorderBtn} ${styles.btn}`}
+          onClick={onReOrderClickHandler}
+        >
+          Determine Re-Order Cost
+        </button>
       </div>
-      {/* 
-      TODO: Add event handlers to these buttons that use the Java API to perform their relative actions.
-      */}
-      <button onClick={onLowStockClickHandler}>Get Low-Stock Items</button>
-      <button onClick={onReOrderClickHandler}>Determine Re-Order Cost</button>
     </>
   );
 }
